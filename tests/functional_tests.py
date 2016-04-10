e381f82fe8e79579a84fda6af382316d11cf53bf
PopWeasel <matloob.qureshi@gmail.com>
2016-04-10 18:38:24 +0100

    using unittests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class StartupTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_startPage(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('Festival Planner', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
