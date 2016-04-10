f78ec50995c3e55f60f58e42f036632cc6cdeb3f
PopWeasel <matloob.qureshi@gmail.com>
2016-04-10 19:25:10 +0100

    added url mapping, a simple view and some unit tests

--- FestivalPlanner/urls.py
+++ FestivalPlanner/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from Indietracks import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.homepage, name='home')
+    #url(r'^admin/', admin.site.urls),
 ]
--- Indietracks/tests.py
+++ Indietracks/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from Indietracks.views import homepage
 
 # Create your tests here.
+class Homepage(TestCase):
+
+    def testHomepageURL(self):
+        found = resolve("/")
+        self.assertEqual(found.func, homepage)
+
+    def testHomepageContent(self):
+        request = HttpRequest()
+        response = homepage(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'Festival Planner', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
